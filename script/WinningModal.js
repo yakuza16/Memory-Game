8d02f39e3ec08f50d329dfb673588fef4dc2a0cf
yakuza16 <marcin.cincin9010@gmail.com>
2020-12-31 11:08:50 +0100

    feat added counting clicks

--- script/WinningModal.js
+++ script/WinningModal.js
@@ -1,26 +1,27 @@
 export class WinningModal {
+  modalInit(gameboard, points, time, score) {
+    this.modalCreation(gameboard, points, time, score);
+    this.restartGame();
+  }
 
-    modalInit(gameboard, points, time) {
-        this.modalCreation(gameboard, points, time);
-        this.restartGame()
-    }
+  modalCreation(gameboard, points, time, clicks) {
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    this.modalContent(modal, points, time, clicks);
+    gameboard.appendChild(modal);
+  }
 
-    modalCreation(gameboard, points, time) {
-        const modal = document.createElement('div');
-        modal.classList.add('modal');
-        this.modalContent(modal, points, time);
-        gameboard.appendChild(modal);
-    }
-
-    modalContent(modal, points, time) {
-        modal.innerHTML = `
+  modalContent(modal, points, time, clicks) {
+    modal.innerHTML = `
         <h3 class="modal__header">Gratulations</h3>
-        <p class="modal__info">Your score is <span>${points}</span> points  in <span>${time}</span> seconds.</p>
+        <p class="modal__info">Your score is <span>${points}</span> points in <span>${time}</span> seconds and <span>${clicks}</span> clicks.</p>
         <button data-playAgain class="modal__button">Play again</button>
-        `
-    }
+        `;
+  }
 
-    restartGame() {
-        document.querySelector('[data-playAgain]').addEventListener('click', () => location.reload());
-    }
+  restartGame() {
+    document
+      .querySelector("[data-playAgain]")
+      .addEventListener("click", () => location.reload());
+  }
 }
